25a417eeced87206ceb56aa2b86f80d025607fe3
Natalia Tymczyszyn <natalia.ewa.tymczyszyn@gmail.com>
2023-10-18 13:38:26 +0200

    add shutter plot

--- projekt1/wave.py
+++ projekt1/wave.py
@@ -1,23 +1,29 @@
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
+import random
 
-# Parameters of the triangular signal
+# parameters 
 frequency = 0.005  # Frequency of the signal (cycles per second)
 amplitude = 0.5  # Amplitude of the signal
 duration = 1000.0  # Duration of the signal (seconds)
 sampling_frequency = 1000  # Sampling frequency (samples per second)
 
-# Generate a time vector
-t = np.linspace(0, duration, int(sampling_frequency * duration), endpoint=False)
+# time vector
+#t = np.linspace(0, duration, int(sampling_frequency * duration), endpoint=False)
+t = np.linspace(0, duration, 100, endpoint=False)
 
-# Generate the triangular signal
+# triangular signal
 triangular_signal = amplitude * (2 * np.abs(2 * (t * frequency - np.floor(t * frequency + 0.5))) - 1)
+test = 2 * np.abs(2 * (t * frequency - np.floor(t * frequency + 0.5))) - 1
+c = 1
+noise = c *(random.random() + random.random() - 1)
 
-# Plot 
 plt.figure()
-plt.plot(t, triangular_signal)
+plt.plot(t, triangular_signal, 'r')
+plt.scatter(t, triangular_signal, c='b', marker='o')
 plt.title('Triangular Signal')
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
 plt.grid(True)
+plt.ylim(-amplitude - 0.5, amplitude + 0.5) 
 plt.show()
