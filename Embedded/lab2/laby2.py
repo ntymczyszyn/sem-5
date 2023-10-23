5ab4a048d9947847588be78438a5a80b084600b2
kifoo <magdaa2622@gmail.com>
2023-10-23 18:29:04 +0200

    embedded

---
+++ Embedded/lab2/laby2.py
@@ -0,0 +1,52 @@
+def decimal_to_binary(decimal):
+    binary = ""
+    if decimal == 0:
+        return "0"
+    while decimal > 0:
+        remainder = decimal % 2
+        binary = str(remainder) + binary
+        decimal = decimal // 2
+    return binary
+
+def decimal_to_hexadecimal(decimal):
+    hexadecimal = ""
+    if decimal == 0:
+        return "0"
+    while decimal > 0:
+        remainder = decimal % 16
+        if remainder < 10:
+            hexadecimal = str(remainder) + hexadecimal
+        else:
+            hexadecimal = chr(ord('A') + remainder - 10) + hexadecimal
+        decimal = decimal // 16
+    return hexadecimal
+
+def main():
+    try:
+        decimal_number = int(input("Podaj liczbę dziesiętną: "))
+        if decimal_number < 0:
+            print("Wprowadzona liczba musi być nieujemna.")
+            return
+
+        system_choice = input("Wybierz system liczbowy (2 - dwójkowy, 16 - szesnastkowy): ")
+
+        if system_choice == "2":
+            binary_number = decimal_to_binary(decimal_number)
+            print(f"Wartość w systemie dwójkowym: {binary_number}")
+        elif system_choice == "16":
+            hexadecimal_number = decimal_to_hexadecimal(decimal_number)
+            print(f"Wartość w systemie szesnastkowym: {hexadecimal_number}")
+        else:
+            print("Niepoprawny wybór systemu liczbowego.")
+
+    except ValueError:
+        print("Wprowadzono niepoprawną liczbę dziesiętną.")
+
+
+if __name__ == "__main__":
+    on = True
+    while(on):
+        main()
+        k = input("Kontynuowac? (T/N) : ") 
+        if k == 'N' or k == 'n':
+            on = False
