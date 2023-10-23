1aea2409fdccba5b420d76aeec7ac1d9154dd7d2
kifoo <magdaa2622@gmail.com>
2023-10-23 17:19:42 +0200

    add MSE

--- projekt1/wave.py
+++ projekt1/wave.py
@@ -20,17 +20,23 @@ H = 2
 noise = [ c *(random.random() + random.random() - 1) for _ in range(len(t))]
 triangular_signal_with_noise = [triangular_signal[i] + noise[i]  for i in range(len(t))]
 
-# the estimated value of the unknown variable
+# the estimated value of the unknow Q variable
+# adds H elements to get sum
 sum_Q = np.zeros(len(t))
 for i in range(len(t)):
     for j in range(H):
         if i > H:
-            sum_Q[i] += triangular_signal_with_noise[i-j]
-# I'm not sure if it works like I want it to 
-unknow_Q = np.zeros(len(t))
-unknow_Q =[ sum_Q[i]/(H) if (i > H) else 0.5 for i in range(len(t))]
+            sum_Q[i] += triangular_signal_with_noise[i-j] 
+estimated_Q = np.zeros(len(t))
+# the average sum to get the estimated value of Q
+estimated_Q =[ sum_Q[i]/(H) if (i > H) else 0.5 for i in range(len(t))]
 
-   
+# the difference between Estimated Q and given value
+MSE = 0
+for i in range(len(t)):
+    MSE += (triangular_signal_with_noise[i] - estimated_Q[i])*(triangular_signal_with_noise[i] - estimated_Q[i]) 
+MSE = MSE/len(t)
+print(MSE)
 """
 plt.figure()
 plt.plot(t, triangular_signal, c='r') 
@@ -44,5 +50,5 @@ plt.show()
 """
 plt.figure()
 plt.plot(t, triangular_signal, c='r')
-plt.plot(t, unknow_Q, c='b', marker='o')
+plt.plot(t, estimated_Q, c='b', marker='o')
 plt.show()
