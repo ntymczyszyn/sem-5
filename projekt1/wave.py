4af53cbfeed940bd05a09332a95148be2385ba58
Natalia Tymczyszyn <natalia.ewa.tymczyszyn@gmail.com>
2023-10-23 15:34:35 +0200

    add signal with noise

--- projekt1/wave.py
+++ projekt1/wave.py
@@ -4,23 +4,24 @@ import random
 
 # parameters 
 frequency = 0.005  # Frequency of the signal (cycles per second)
-amplitude = 0.5  # Amplitude of the signal
+amplitude = 1.0  # Amplitude of the signal
 duration = 1000.0  # Duration of the signal (seconds)
-sampling_frequency = 1000  # Sampling frequency (samples per second)
+sampling_frequency = 0.15  # Sampling frequency (samples per second)
 
 # time vector
-#t = np.linspace(0, duration, int(sampling_frequency * duration), endpoint=False)
-t = np.linspace(0, duration, 100, endpoint=False)
+t = np.linspace(0, duration, int(sampling_frequency * duration), endpoint=False) # <class 'numpy.ndarray'>
 
-# triangular signal
+# triangular signal - is it corect computation? 
 triangular_signal = amplitude * (2 * np.abs(2 * (t * frequency - np.floor(t * frequency + 0.5))) - 1)
-test = 2 * np.abs(2 * (t * frequency - np.floor(t * frequency + 0.5))) - 1
-c = 1
-noise = c *(random.random() + random.random() - 1)
+#test = 2 * np.abs(2 * (t * frequency - np.floor(t * frequency + 0.5))) - 1
+
+c = 0.3
+noise = [ c *(random.random() + random.random() - 1) for _ in range(len(t))]
+triangular_signal_with_noise = [triangular_signal[i] + noise[i]  for i in range(len(t))]
 
 plt.figure()
-plt.plot(t, triangular_signal, 'r')
-plt.scatter(t, triangular_signal, c='b', marker='o')
+plt.plot(t, triangular_signal, c='r') 
+plt.scatter(t, triangular_signal_with_noise, c='b', marker='o')
 plt.title('Triangular Signal')
 plt.xlabel('Time (s)')
 plt.ylabel('Amplitude')
