b3bdd88f165bdfad33adadfac9cd1037a815a4f4
Kifoo <magdaa2622@gmail.com>
2023-11-22 21:29:44 +0100

    Embedded lab5 update

--- Embedded/lab5/lab5.py
+++ Embedded/lab5/lab5.py
@@ -1,123 +1,129 @@
-# from machine import Pin
+from machine import Pin
 import time
-from numpy import char
-
-# Do przycisków:
+# Do przycisków: ???
 # przycisk_pin = Pin(0, Pin.IN, Pin.PULL_UP)  # Ustawienie pinu przycisku (dla przykładu użyto pinu numer 0)
-
-# while True:
-#     if przycisk_pin.value() == 0:
-#         print("Przycisk został naciśnięty.")
-
+# Funkcja obsługująca naciśnięcie przycisku
+# def obsluz_przycisk(pin):
+#     print("Przycisk został naciśnięty!")
+# # Przypisz funkcję obsługującą do przerwania na zmianę stanu pinu (naciśnięcie przycisku)
+# przycisk_pin.irq(trigger=Pin.IRQ_FALLING, handler=obsluz_przycisk)
 # USTAWIENIE PINOW
 joints_OFF =  [[1,5],[2,8]]
 joint_ON = [[1,6],[2,7]]
-
-
+LED = Pin(2, Pin.OUT) # LED = Pin('Y12',Pin.OUT)
+# LED_2 = Pin(0, Pin.OUT) 
+# joint_off1 = [Pin(1, Pin.OUT),Pin(5, Pn.OUT)] ??
+# joint_off2 = [Pin(2, Pin.OUT),Pin(8, Pin.OUT)]
+# joint_on1 =  [Pin(1, Pin.OUT),Pin(6, Pin.OUT)]
+# joint_on2 = [Pin(2, Pin.OUT),Pin(7, Pin.OUT)]
 def INFO():
-    print("----- PRZEKAZNIK CZASOWY PCU-520 -----")
-    print("--- SPOSOB DZIALANIA ---")
+    print("\t----- PRZEKAZNIK CZASOWY PCU-520 -----")
+    print("\t ---- SPOSOB DZIALANIA AUTO ----")
     print("Styki pozostaja w pozycji 1-5, 2-8 (OFF) do czasu zalaczenia. Po podaniu napiecia zasilajacego przelaczone zostaja w pozycja 1-6, 2-7(ON).")
     print("A - OPOZNIONE WYLACZENIE \nDo czasu zalaczenia przekaznika styki pozostaja w pozycji 1-5, 2-8.\nPo podaniu napiecia zasilajacego styki zostaja przelaczone w pozycje 1-6, 2-7 na czas t1. \nPo odmierzeniu czasu t1 styki powracaja do pozycji 1-5, 2-8 na czas t2. \nPo czasie t2 styki przekaznika powracaja do pozycji 1-5, 2-8 na czas t2. Po czasie t2 styki przekaznika powracaja do pozycji 1-6, 2-7. \nPonowna realizacja trybu pracy przekaznika mozliwa jest po odlaczeniu napiecia zasilajacego i ponownym jego zalaczeniu.")
     print("B - OPOZNIONE ZALACZENIE \nPo podaniu napiecia zasilajacego styki pozostaja w pozycji 1-5, 2-8 przez czas t1. \nPo odmierzeniu czasu t1 nastepuje przelaczenie stykow w pozycje 1-6, 2-7 na czas t2. \nPo czasie t2 styki przekaznika powracaja do pozycji 1-5, 2-8. \nPonowna realizacja trybu pracy przekaznika mozliwa jest po odlaczeniu napiecia zasilajacego i ponownym jego zalaczeniu.")
     print("C - OPOZNIONE WYLACZENIE - CYKLICZNIE \nTryb pracy opoznionego wylaczania realizowany cyklicznie w ustawionych odstepach czasu pracy i przerwy.")
     print("D - OPOZNIONE ZALACZENIE - CYKLICZNIE \nTryb pracy opoznionego zalaczania realizowany cyklicznie w ustawionych odstepach czasu pracy i przerwy.") 
     return
-
 def MENU():
     try:
         choice = int(input("TRYBY PRACY: \n1. AUTO \n2.MANUAL_BI \n3.MANUAL_MONO \nWybor: "))
     except ValueError:
         print("ValueError - wprowadzona wartosc nie jest liczba naturalna, sprobuj ponownie.")
         return
-
-    if ( choice == 1 ):
-        try:
-            print("FUNCJE: \nA. Opoznione wylaczanie \nB. Opoznione zalaczanie \nC. Opoznione wylaczenie-cykliczne \nD. Opoznione zalaczenie-cykliczne")
-            auto_choice = (input("Wybor: "))
-        except ValueError:
-            print("ValueError - wprowadzona wartosc jest nieprawidłowa")
-            return 
-        return auto_choice
-    elif ( choice == 2 or choice == 3):
+    if ( choice == 1 or choice == 2 or choice == 3):
         return choice
     else:
         print("Blednie dane, sprobuj ponownie")
         return MENU() # Mozna tak?
-
-def LED_PULS():
-    for i in range(0,120):
-        # zapal/zgas diode
-        time.sleep(0.01)
-        #zgas/ zapal diode
-        time.sleep(0.01)
-
-def A():
+def AUTO():
     try:
-        t1 = float(input("Podaj t1 [3s - 20s]: ")) 
-        t2 = float(input("Podaj t2 [3s - 20s]: "))
+        print("FUNCJE: \nA. Opoznione wylaczanie \nB. Opoznione zalaczanie \nC. Opoznione wylaczenie-cykliczne \nD. Opoznione zalaczenie-cykliczne")
+        choice = str(input("Wybor: "))
+    except ValueError:
+        print("ValueError - wprowadzona wartosc jest nieprawidłowa")
+        return 
+    try:
+        t1 = float(input("Podaj t1 [3s - 12s]: ")) 
+        t2 = float(input("Podaj t2 [3s - 12s]: "))
     except ValueError:
         print("ValueError - wprowadzona wartosc nie niepoprawna, sprobuj ponownie.")
         return
-    if( t1 < 2 or t2 < 2):
+    if (t1 > 2 or t2 > 2):
+        if choice == 'A' or choice == 'a':
+            A(t1, t2)
+        elif choice == 'B' or choice == 'b':
+            B(t1, t2)
+        elif choice == 'C' or choice == 'c':
+            C(t1, t2)
+        elif choice == 'D' or choice == 'd':
+            D(t1, t2)
+    else:
         print("Za male wartosci")
+def LED_PULS():
+    for i in range(10):
+        # zapal/zgas diode1
+        LED.value(not LED.value())
+        # time.sleep_ms(100)
+        time.sleep(0.1)
+        #zgas/ zapal diode
+        LED.value(not LED.value())
+        time.sleep(0.1)
+def A(t1, t2):        
     # zaczynamy od stykow na joint_OFF
     start = input("Praca rozpocznie się po wcisnieciu przycisku ...")
     if start == 'a':
         # ustawienie stykow na joint_ON
+        LED.off()
+        print("ON")
         time.sleep(t1-2)
         LED_PULS()
         # ustawienie styków na joint_OFF
+        LED.on()
+        print("OFF")
         time.sleep(t2-2)
         LED_PULS
         # ustawienie stykow na joint_ON
-        print("ok")
+        LED.off()
+        print("ON")
         return
-
-def B():
-    try:
-        t1 = float(input("Podaj t1 [3s - 20s]: ")) 
-        t2 = float(input("Podaj t2 [3s - 20s]: "))
-    except ValueError:
-        print("ValueError - wprowadzona wartosc nie niepoprawna, sprobuj ponownie.")
-        return
+def B(t1, t2):
     # zaczynamy od stykow na joint_OFF
     start = input("Praca rozpocznie się po wcisnieciu przycisku ...")
     if start == 'a':
         # styki na joint_OFF
-        time.sleep(t1)
+        LED.on()
+        print("OFF")
+        time.sleep(t1-2)
+        LED_PULS()
         # ustawienie styków na joint_ON
-        time.sleep(t2)
+        LED.off()
+        print("ON")
+        time.sleep(t2-2)
+        LED_PULS()
         # ustawienie stykow na joint_OFF
+        LED.on()
+        print("ON")
         return
-
-def C():
-    try:
-        t1 = float(input("Podaj t1 [3s - 20s]: ")) 
-        t2 = float(input("Podaj t2 [3s - 20s]: "))
-    except ValueError:
-        print("ValueError - wprowadzona wartosc nie niepoprawna, sprobuj ponownie.")
-        return
+def C(t1, t2):
     # zaczynamy od stykow na joint_OFF
     start = input("Praca rozpocznie się po wcisnieciu przycisku ...")
     if start == 'a':
         i=0 # do zmiany na przycisk ??
         while(i < 10):
             # ustawienie stykow na joint_ON
-            time.sleep(t1)
+            LED.off()
+            print("ON")
+            time.sleep(t1-2)
+            LED_PULS()
             # ustawienie styków na joint_OFF
-            time.sleep(t2)
-            # ustawienie stykow na joint_ON
+            LED.on()
+            print("OFF")
+            time.sleep(t2-2)
+            LED_PULS()
             i += 1
         return
-
-def D():
-    try:
-        t1 = float(input("Podaj t1 [3s - 20s]: ")) 
-        t2 = float(input("Podaj t2 [3s - 20s]: "))
-    except ValueError:
-        print("ValueError - wprowadzona wartosc nie niepoprawna, sprobuj ponownie.")
-        return
+def D(t1, t2):
     # zaczynamy od stykow na joint_OFF
     start = input("Praca rozpocznie się po wcisnieciu przycisku ...")
     # ustawienie stykow na joint_ON
@@ -125,49 +131,45 @@ def D():
         i=0 # do zmiany na przycisk ??
         while(i < 10):
             # styki sa na joint_OFF
-            time.sleep(t1)
+            LED.on()
+            print("OFF")
+            time.sleep(t1-2)
+            LED_PULS()
             # ustawienie stykow na joint_ON
-            time.sleep(t2)
-            # ustawienie styków na joint_OFF
+            LED.off()
+            print("ON")
+            time.sleep(t2-2)
+            LED_PULS()
             i += 1
         return
-
 def MANUAL_BI():
     i = 0 # do zmiany na przycisk ??
     while(i < 10):
         i += 1
         if( True):
+            # LED.value(not led.value())
             print("zmiana stanu")
-
     return
-
 def MANUAL_MONO():
     i = 0 # do zmiany na przycisk ??
     while(i < 10):
         i += 1
         if( True):
-            print("zasilanie przez 1 s")
+            # LED.off
+            print("ON przez 1s")
             time.sleep(1)
+            print("ON")
     return
-
 def driver():
     choice = MENU()
-    if choice == 'A' or choice == 'a':
-        A()
-    elif choice == 'B' or choice == 'b':
-        B()
-    elif choice == 'C' or choice == 'c':
-        C()
-    elif choice == 'D' or choice == 'd':
-        D()
+    if choice == 1:
+        AUTO()
     elif choice == 2:
         MANUAL_BI()
     elif choice == 3:
         MANUAL_MONO()
     else:
         print("Nieprawidłowy wybór.")
-
-
 def main():
     INFO()
     driver()
--- adaptacyjne/projekt2/wave.py
+++ adaptacyjne/projekt2/wave.py
@@ -57,96 +57,6 @@ def estimate(triangular_signal, N, c, H):
     return triangular_signal_with_noise, estimated, MSE
 
 
-def images_MSE(t, N, max, triangular_signal, c_index, H_index):
-    H, MSE, var, H_opt, MSE_opt = calculate_all(N, max, triangular_signal)
-    # MSE(H) and MSE(var) plots
-    fig_H, (ax_H, ax_var) = plt.subplots(1, 2, figsize=(12, 6))
-    #  H
-    fig_H = plt.figure(figsize=(8, 6))
-    ax_H = fig_H.add_subplot(111)
-    ax_H.scatter(H, MSE[c_index,:], color='b', marker='o', s=10)
-    ax_H.set_title(f'Zależność MSE(H) dla var(Z) ={var[c_index]:.2f}')
-    ax_H.set_xlabel('Horyzont pamięci')
-    ax_H.set_ylabel('MSE(H)')
-    # var
-    fig_var = plt.figure(figsize=(8, 6))
-    ax_var = fig_var.add_subplot(111)
-    ax_var.scatter(var, MSE[: , H_index] , color='r', marker='o', s=10)
-    ax_var.set_title(f'Zależność MSE(H) dla H = {H[H_index]} przy różnych wariacjach var(Z)')
-    ax_var.set_xlabel('Wariancja')
-    ax_var.set_ylabel('MSE(H)')
-
-    # Hopt(var) 
-    fig_opt = plt.figure(figsize=(8, 6))
-    ax_opt = fig_opt.add_subplot(111)
-    ax_opt.scatter(var, H_opt,  c='r', s=10) 
-    ax_opt.set_title('Zależność H optymalnego od wariancji var(Z)')
-    ax_opt.set_ylabel('Optymalny horyzont pamięci')
-    ax_opt.set_xlabel('Wariancja')
-    ax_opt.set_ylim(0, max)
-
-    # 3d plot - H, var, MSE(h) + h opt for all calculated var
-    fig_3d = plt.figure(figsize=(14, 6))
-    ax0 = fig_3d.add_subplot(121, projection='3d')
-    ax1 = fig_3d.add_subplot(122, projection='3d')
-    X, Y = np.meshgrid(H, var)
-    Z = MSE
-
-    # optimal horizon plot
-    ax1.scatter(H_opt, var, MSE_opt, c='#fe008b', marker='o', s=40, label="H_opt(var(Z))")
-    colors = cm.viridis(Z)
-    rcount, ccount, _ = colors.shape
-    # change in
-    ax0.plot_surface(X, Y, Z, cmap='viridis')
-    ax0.set_xlabel('Horyzont pamięci')
-    ax0.set_ylabel('Wariancja')
-    ax0.set_zlabel('MSE(H)')
-    ax0.set_title('MSE(H) przy różnych wariancjach var(Z)')
-    ax0.view_init(elev=20, azim=-120)
-
-    surf = ax1.plot_surface(X, Y, Z, rcount=rcount, ccount=ccount, facecolors=colors, shade=False)
-    surf.set_facecolor((0,0,0,0))
-    ax1.set_xlabel('Horyzont pamięci')
-    ax1.set_ylabel('Wariancja')
-    ax1.set_zlabel('MSE(H)')
-    ax1.set_title('MSE(H) przy różnych wariancjach var(Z)')
-    ax1.legend(loc='center left', bbox_to_anchor=(1, 1))
-    ax1.view_init(elev=20, azim=-120)
-    plt.tight_layout()
-
-    fig_3d.savefig(os.path.join(os.path.dirname(__file__), "images", "Wykres_3d.png"), dpi=500) # APPROVED
-    fig_opt.savefig(os.path.join(os.path.dirname(__file__), "images", "Wykres_opt.png"), dpi=500) # APPROVED
-    fig_H.savefig(os.path.join(os.path.dirname(__file__), "images", f"Wykres_H_c={var[c_index]:.2f}.png"), dpi=500) # APPROVED
-    fig_var.savefig(os.path.join(os.path.dirname(__file__), "images", f"Wykres_var_H={H[H_index]}.png"), dpi=500) # APPROVED
-
-    fig_3d.show()
-    g = input("STOP")
-
-# signal w/ and wo/ noise 
-def images_signal(t, triangular_signal, N, c_value, H_value, amplitude):
-    t_w_n, est, MSE = estimate(triangular_signal, N, c=c_value, H=H_value)
-    
-    fig = plt.figure(figsize=(14, 6))
-    ax = fig.add_subplot(121) 
-    ax_est = fig.add_subplot(122)
-    ax.plot(t, triangular_signal, c='r', label="Prawdziwa fala trójkątna") 
-    ax.scatter(t, t_w_n, c='g', marker='o', label="Zaszumiona fala trójkątna")
-    var = 2 *(c_value**2)/3
-    ax.set_title(f'Prawdziwa i zaszumiona fala trójkątna dla var(Z) = {var:.2f}')
-    ax.legend(loc='upper right')
-    ax.set_xlabel('Czas (s)')
-    ax.set_ylabel('Amplituda')
-    ax.set_ylim(-amplitude - c_value - 0.1, amplitude + c_value + 0.1)
-    ax_est.plot(t, triangular_signal, c='r', label="Prawdziwa fala trójkątna") 
-    ax_est.plot(t[H_value+1:], est[H_value+1:], c='b', marker='o', label="Estymowana fala trójkątna")
-    ax_est.set_title(f'Prawdziwa fala trójkątna i jej estymacja dla H = {H_value}')
-    ax_est.legend(loc='upper right')
-    ax_est.set_xlabel('Czas (s)')
-    ax_est.set_ylabel('Amplituda')
-    ax_est.set_ylim(-amplitude - c_value - 0.1, amplitude + c_value + 0.1)
-    fig.savefig(os.path.join(os.path.dirname(__file__), "estimate", f"Szum_c={c_value}__H={H_value}_MSE={MSE:.3f}.png"), dpi=500) # APPROVED
-
-
 def main():
     # triangular function parameters 
     frequency = 0.01  # Frequency of the signal (cycles per second)
@@ -158,19 +68,16 @@ def main():
     # time vector
     t = np.linspace(0, duration, int(sampling_frequency * duration), endpoint=False) # <class 'numpy.ndarray'>
     N = len(t)
+
     triangular_signal = ((4 * amplitude) / period) * np.abs(((t - (period * 0.25))%period) - (period * 0.5)) - amplitude
-    
-    # amount of samples (H and var)
-    max = 80
-    c_value = [0.2, 0.4, 0.6, 0.8, 1]
-    H_value = [2, 5, 7, 15, 25, 50]
-    for i in range(len(c_value)):
-        for j in range(len(H_value)):
-            images_signal(t, triangular_signal, N, c_value[i], H_value[j], amplitude)
-    
-    c_index = 40
-    H_index = 4 
-    images_MSE(t, N, max, triangular_signal, c_index, H_index)
+
+    H, MSE, var, H_opt, MSE_opt = calculate_all(N, max, triangular_signal)
+    t_w_n, est, MSE = estimate(triangular_signal, N, c=0.4, H=5)
+
+    # u_true = [u_0, u_1, u_3]
+    # b_true = [b_0, b_1, b_2]
+    # V_k = b_true[0]*u_true[0] + b_true[1]*u_true[1] + b_true[2]*u_true[2]
+    # y_k = v_k + z_k
     
     
 if __name__ == "__main__":
