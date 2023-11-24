b0ae131b31aa699ddb0e29140eab235b47dbfe0d
Kifoo <magdaa2622@gmail.com>
2023-11-24 18:58:43 +0100

    P2 simulation work

--- adaptacyjne/projekt2/wave.py
+++ adaptacyjne/projekt2/wave.py
@@ -6,87 +6,103 @@ from matplotlib import cm
 
 C = 0.7
 
-def preparing_true_data(u_k, t, b_true):
-    N = len(t)
-    V_true = np.zeros(N)
-    y_true = np.zeros(N)
-    noise = np.zeros(N)
-    for k in range(2, N):
-        noise[k] = C *(random.random() + random.random() - 1)
-        V_true[k] =  (b_true[0]*u_k[k] + b_true[1]*u_k[k-1] + b_true[2]*u_k[k-2])
-        y_true[k] = V_true[k] + noise[k]
-    return y_true, V_true, noise
+# def preparing_true_data(u_k, t, b_true):
+#     N = len(t)
+#     V_true = np.zeros(N)
+#     y_true = np.zeros(N)
+#     noise = np.zeros(N)
+#     for k in range(2, N):
+#         noise[k] = C *(random.random() + random.random() - 1)
+#         V_true[k] =  (b_true[0]*u_k[k] + b_true[1]*u_k[k-1] + b_true[2]*u_k[k-2])
+#         y_true[k] = V_true[k] + noise[k]
+#     return y_true, V_true, noise
 
-def simulation(u_k, t,  b_true, Lambda):
-    N = len(t)
-    y_k, V_true, noise = preparing_true_data(u_k, t, b_true)
-    
+def simulation(N, b_true, Lambda):    
     P_k = np.array([[100, 0, 0],
                     [0, 100, 0],
                     [0, 0, 100]])
 
-    b_est0 = np.zeros(N)
-    b_est1 = np.zeros(N)
-    b_est2 = np.zeros(N)
     b_estimated = np.zeros((3,N))
-    # b_estimated = np.array([[b_est0[0]],
-    #                         [b_est1[0]],
-    #                         [b_est2[0]]])
     
-    V_k = np.zeros(N)
-    y_k_estimated = np.zeros(N)
-    y_k_estimated = y_k
-    MSE_sum = 0
-    MSE_sum2 = 0
-    for k in range(3, N):
-        Phi = np.array([[u_k[k]],
-                        [u_k[k-1]],
-                        [u_k[k-2]]])
+    V_estimated = np.zeros(N)
+    y_estimated = np.zeros(N)
+
+    V_true = np.zeros(N) # prawdziwe wyjście z obiektu bez zakłócenia
+    y_true = np.zeros(N) # prawdziwe wyjście z zakłóceniem
+
+    # Tworzenie zakłócenia
+    noise = [C *(random.random() + random.random() - 1) for _ in range(N)]
+    # Tworzenie danych przed pętlą 
+    # Dla [0]
+    u_calculated = np.zeros(N)
+    u_calculated[0] = (np.sin(2*np.pi/3 * 0) + 1.5)
+    Phi = np.array([[u_calculated[0]],
+                    [0],
+                    [0]])
+    V_true[0] = b_true[0]*u_calculated[0]
+    y_true[0] = V_true[0]  + noise[0]
+    new_P_k = 1/Lambda * (P_k - (np.dot(np.dot(np.dot(P_k,Phi),Phi.T),P_k))/ (Lambda + np.dot(np.dot(Phi.T, P_k),Phi)))
+    P_k = new_P_k
+
+    B_estimated = [ [0],
+                    [0],
+                    [0]]
+    b_estimated[:,0] = (B_estimated + (y_true[0] - np.dot(Phi.T, B_estimated)) * np.dot(P_k,Phi)).reshape((3,)) 
+
+    # DLa [1]
+    u_calculated[1] = (np.sin(2*np.pi/3 * 1) + 1.5)/b_estimated[0, 0]
+    Phi = np.array([[u_calculated[1]],
+                    [u_calculated[0]],
+                    [0]])
+
+    V_true[1] = b_true[0]*u_calculated[1] + b_true[1]*u_calculated[0] 
+    y_true[1] =  V_true[1] + noise[1]
+    new_P_k = 1/Lambda * (P_k - (np.dot(np.dot(np.dot(P_k,Phi),Phi.T),P_k))/ (Lambda + np.dot(np.dot(Phi.T, P_k),Phi)))
+    P_k = new_P_k
+
+    B_estimated = [ [b_estimated[0, 0]],
+                    [b_estimated[1, 0]],
+                    [b_estimated[2, 0]]]
+    b_estimated[:,1] = (B_estimated + (y_true[1] - np.dot(Phi.T, B_estimated)) * np.dot(P_k,Phi)).reshape((3,)) 
+
+    for k in range(2, N):
+        u_calculated[k] = ((np.sin(2*np.pi/3 * k) + 1.5) -  (b_estimated[1, k-1]* u_calculated[k-1] + b_estimated[2, k-1]*u_calculated[k-2]))/b_estimated[0, k-1]
+        Phi = np.array([[u_calculated[k]],
+                        [u_calculated[k-1]],
+                        [u_calculated[k-2]]])
+
+        # Wyliczamy nowe wyjście na podstawie oczekiwanego wejścia?? z b_true
+        V_true[k] =  (b_true[0]*u_calculated[k] + b_true[1]*u_calculated[k-1] + b_true[2]*u_calculated[k-2])
+        y_true[k] = V_true[k] + noise[k]
 
         # Calculate P_k - macierz kowariancji P_k to P(k-1) a new_P_k to P(k)
         new_P_k = 1/Lambda * (P_k - (np.dot(np.dot(np.dot(P_k,Phi),Phi.T),P_k))/ (Lambda + np.dot(np.dot(Phi.T, P_k),Phi)))
         P_k = new_P_k
 
-        # Wyliczamy ze starym b nowe wyjście
-        
         # Calculate b_estimated 
         B_estimated = [[b_estimated[0, k-1]],
                        [b_estimated[1, k-1]],
                        [b_estimated[2, k-1]]]
-        b_estimated[:,k] = (B_estimated + (y_k[k] - np.dot(Phi.T, B_estimated)) * np.dot(P_k,Phi)).reshape((3,)) 
+        b_estimated[:,k] = (B_estimated + (y_true[k] - np.dot(Phi.T, B_estimated)) * np.dot(P_k,Phi)).reshape((3,)) 
+            
+        V_estimated[k] =  (b_estimated[0, k]*u_calculated[k] + b_estimated[1, k]*u_calculated[k-1] + b_estimated[2, k]*u_calculated[k-2])
+        y_estimated[k] = V_estimated[k] + noise[k]
 
-        V_k[k] =  (b_estimated[0][k]*u_k[k] + b_estimated[1][k]*u_k[k-1] + b_estimated[2][k]*u_k[k-2])
-        y_k_estimated[k] = V_k[k] + noise[k]
-        b_est0[k] = b_estimated[0][k]
-        b_est1[k] = b_estimated[1][k]
-        b_est2[k] = b_estimated[2][k]
-        # V_k[k] =  (b_est0[k]*u_k[k] + b_est1[k]*u_k[k-1] + b_est2[k]*u_k[k-2])
-        # y_k_estimated[k] = V_k[k] + noise[k]
-
-        MSE_sum += (np.abs(V_k[k] - V_true[k]))**2
-        MSE_sum2 += (np.abs(y_k_estimated[k] - y_k[k]))**2
-
-
-    MSE = MSE_sum/(N-2)
-    MSE2 = MSE_sum2/(N-2)
-    print(f"MSE dla Lambdy = {Lambda} : {MSE}")
-    print(f"MSE2 dla Lambdy = {Lambda} : {MSE2}")
-    return V_k, V_true, y_k_estimated, y_k, b_est0, b_est1, b_est2
+    print(f"B = {b_estimated[:,49]}")    
+    return V_true, V_estimated, y_true, y_estimated, b_estimated
     
 
-def image(t, u_k, b_true, Lambda):
-    V_k, V_true, estimated_with_noise, y_k, b_est0, b_est1, b_est2 = simulation(u_k, t,  b_true, Lambda)
+def image(t, b_true, Lambda):
+    V_true, V_estimated, y_true, y_estimated, b_estimated = simulation(len(t), b_true, Lambda)
     fig = plt.figure(figsize=(14, 6))
     ax = fig.add_subplot(111)
-    ax.plot(t, V_k, c='b', label="Estymowana fala") 
-    ax.plot(t, V_true, c='r', label="Fala trójkątna bez szumu")
-    ax.scatter(t, V_k, c='b', marker="o", s=10, label="Estymowana fala") 
-    # ax.scatter(t, V_true, c='r', marker="o", s=10, label="Fala trójkątna bez szumu")
-    # ax.scatter(t, estimated_with_noise, c='r', marker="o", s=10, label="Estymowana fala + szum")  
-    # ax.scatter(t, y_k, c='g', marker="o", s=10, label="Zaszumiona fala trójkątna")
-    # ax.plot(t, estimated_with_noise, c='r', label="Estymowana fala + szum")  
-    # ax.plot(t, y_k, c='g', label="Zaszumiona fala trójkątna")
-    ax.set_title(f'Estymowana i zaszumiona fala dla Lamby = {Lambda}')
+    ax.plot(t, V_true, c='b', label="Fala na wyjściu -> prawdziwa") 
+    ax.plot(t, V_estimated, c='r', label="Estymowana fala")
+    ax.scatter(t, V_estimated, c='r', marker="o", s=10) 
+    # ax.plot(t, y_true, c='g', label="Fala na wyjściu + szum -> prawdziwa")
+    # ax.plot(t, y_estimated, c='r', label="Estymowana fala + szum")  
+    # ax.scatter(t, y_estimated, c='r', marker="o", s=10, label="Estymowana fala + szum")  
+    ax.set_title(f'Prawdziwa i estymowana fala bez szumu dla Lamby = {Lambda}')
     ax.legend(loc='upper right')
     ax.set_xlabel('Czas (s)')
     ax.set_ylabel('Amplituda')
@@ -95,13 +111,13 @@ def image(t, u_k, b_true, Lambda):
     
     fig_est = plt.figure(figsize=(14, 6))
     ax_est = fig_est.add_subplot(111)
-    ax_est.scatter(t, b_est0, c='r', marker="o", s=10, label="Estymowane b0 od Lambdy")  
-    ax_est.plot(t, [b_true[0] for i in range(len(t))], c='black') 
-    ax_est.scatter(t, b_est1, c='b', marker="o", s=10, label="Estymowane b1 od Lambdy")
-    ax_est.plot(t, [b_true[1] for i in range(len(t))], c='black') 
-    ax_est.scatter(t, b_est2, c='g', marker="o", s=10, label="Estymowane b2 od Lambdy") 
-    ax_est.plot(t, [b_true[2] for i in range(len(t))], c='black') 
-    ax_est.set_title(f'Estymowana i zaszumiona fala dla Lamby = {Lambda}')
+    ax_est.scatter(t, b_estimated[0], c='r', marker="o", s=10, label="Estymowane b0 od Lambdy")  
+    ax_est.plot(t, [b_true[0] for _ in range(len(t))], c='black') 
+    ax_est.scatter(t, b_estimated[1], c='b', marker="o", s=10, label="Estymowane b1 od Lambdy")
+    ax_est.plot(t, [b_true[1] for _ in range(len(t))], c='black') 
+    ax_est.scatter(t, b_estimated[2], c='g', marker="o", s=10, label="Estymowane b2 od Lambdy") 
+    ax_est.plot(t, [b_true[2] for _ in range(len(t))], c='black') 
+    ax_est.set_title(f'Estymacja b^ dla Lambdy = {Lambda}')
     # ax_est.legend(loc='')
     ax_est.set_xlabel('Czas (s)')
     ax_est.set_ylabel('Amplituda')
@@ -117,21 +133,21 @@ def image(t, u_k, b_true, Lambda):
 # SZUKANE
 # b_true -> poprzez b_estimated
 # INNE
-# V_k -> sygnał po wyjściu z obiektu, którym mamy "sterować" 
+# V_estimated -> sygnał po wyjściu z obiektu, którym mamy "sterować" 
 def main():
-    duration = 1000.0  # Duration of the u_k(seconds)
-    N = 50
+    duration = 500.0  # Duration of the u_k(seconds)
+    N = 250
     t = np.linspace(0, duration, N, endpoint=False) # <class 'numpy.ndarray'>
     u_k = np.random.uniform(0, 3,  N)
     #u_k = [ random.random()-0.5 for _ in range(N)]
     # u_k = [1 for i in range(len(t))]
 
-    Lambda = [1]
-    b_0, b_1, b_2 = 2, 3, 1 #17.5, 10, 2.5
+    Lambda = [1, 0.9]
+    b_0, b_1, b_2 = 5, 3, 1 #17.5, 10, 2.5
     b_true = [b_0, b_1, b_2]
     
     for i in range(len(Lambda)):
-        image(t, u_k, b_true, Lambda[i])
+        image(t, b_true, Lambda[i])
 
     # image_MSE(t, u_k, b_true, Lambda)
     
