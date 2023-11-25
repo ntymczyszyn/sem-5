336f0eda6f3fae584c543cdef4a454c9f4a116c4
Kifoo <magdaa2622@gmail.com>
2023-11-25 12:57:51 +0100

    part II b changed

--- adaptacyjne/projekt2/wave.py
+++ adaptacyjne/projekt2/wave.py
@@ -7,8 +7,9 @@ from matplotlib import cm
 C = 0.7 # 
 images_folder = "images_test"
 
-def simulation2(N, b_true, Lambda):
-    B_true = np.zeros((N,3))
+# B_changed to funckja zmiennego b_true[1]
+def simulation2(t, b_true, B_changed, Lambda):
+    N = len(t)
     P_k = np.array([[100, 0, 0],
                     [0, 100, 0],
                     [0, 0, 100]])
@@ -25,14 +26,15 @@ def simulation2(N, b_true, Lambda):
     noise = [C *(random.random() + random.random() - 1) for _ in range(N)]
     # Tworzenie danych przed pętlą 
     # Dla [0]
+    v_wanted = (np.sin(2*np.pi/3 * 0) + 1.5)
     u_calculated = np.zeros(N)
-    u_calculated[0] = (np.sin(2*np.pi/3 * 0) + 1.5)
+    u_calculated[0] = v_wanted
     Phi = np.array([[u_calculated[0]],
                     [0],
                     [0]])
+    b_true[1] = B_changed[0]
     V_true[0] = b_true[0]*u_calculated[0]
     y_true[0] = V_true[0]  + noise[0]
-    B_true[0] = b_true
     new_P_k = 1/Lambda * (P_k - (np.dot(np.dot(np.dot(P_k,Phi),Phi.T),P_k))/ (Lambda + np.dot(np.dot(Phi.T, P_k),Phi)))
     P_k = new_P_k
 
@@ -42,14 +44,14 @@ def simulation2(N, b_true, Lambda):
     b_estimated[:,0] = (B_estimated + (y_true[0] - np.dot(Phi.T, B_estimated)) * np.dot(P_k,Phi)).reshape((3,)) 
 
     # DLa [1]
-    u_calculated[1] = (np.sin(2*np.pi/3 * 1) + 1.5)/b_estimated[0, 0]
+    v_wanted = (np.sin(2*np.pi/3 * 1) + 1.5)
+    u_calculated[1] = v_wanted/b_estimated[0, 0]
     Phi = np.array([[u_calculated[1]],
                     [u_calculated[0]],
                     [0]])
-
+    b_true[1] = B_changed[1]
     V_true[1] = b_true[0]*u_calculated[1] + b_true[1]*u_calculated[0] 
     y_true[1] =  V_true[1] + noise[1]
-    B_true[1] = b_true
     new_P_k = 1/Lambda * (P_k - (np.dot(np.dot(np.dot(P_k,Phi),Phi.T),P_k))/ (Lambda + np.dot(np.dot(Phi.T, P_k),Phi)))
     P_k = new_P_k
 
@@ -60,12 +62,12 @@ def simulation2(N, b_true, Lambda):
 
     for k in range(2, N):
         # Do zmiany b* 
-        if(k % (N/5) == 0):
-            # b_true = [b_true[0] + 1, b_true[1] + 1, b_true[2] + 1
-            b_true[0] = B_true[0][0] +  C *(random.random() + random.random() - 1)
-        B_true[k] = b_true
+        b_true[1] = B_changed[k]
             
-        u_calculated[k] = ((np.sin(2*np.pi/3 * k) + 1.5) -  (b_estimated[1, k-1]* u_calculated[k-1] + b_estimated[2, k-1]*u_calculated[k-2]))/b_estimated[0, k-1]
+        # Dla sygnału trójkatnego:
+        # v_wanted = 
+        v_wanted = (np.sin(2*np.pi/3 * k) + 1.5)
+        u_calculated[k] = (v_wanted -(b_estimated[1, k-1]* u_calculated[k-1] + b_estimated[2, k-1]*u_calculated[k-2]))/b_estimated[0, k-1]
         Phi = np.array([[u_calculated[k]],
                         [u_calculated[k-1]],
                         [u_calculated[k-2]]])
@@ -89,10 +91,10 @@ def simulation2(N, b_true, Lambda):
 
     # print(f"B true = {B_true}")
     print(f"B2 = {b_estimated[:,49]}")    
-    return V_true, V_estimated, y_true, y_estimated, b_estimated, B_true
+    return V_true, V_estimated, y_true, y_estimated, b_estimated
 
-def image2(t, b_true, Lambda):
-    V_true, V_estimated, y_true, y_estimated, b_estimated, B_true = simulation2(len(t), b_true, Lambda)
+def image2(t, b_true, B_changed, Lambda):
+    V_true, V_estimated, y_true, y_estimated, b_estimated= simulation2(t, b_true, B_changed, Lambda)
     fig = plt.figure(figsize=(14, 6))
     ax = fig.add_subplot(111)
     ax.plot(t, V_true, c='b', label="Fala na wyjściu -> prawdziwa") 
@@ -111,11 +113,11 @@ def image2(t, b_true, Lambda):
     fig_est = plt.figure(figsize=(14, 6))
     ax_est = fig_est.add_subplot(111)
     ax_est.scatter(t, b_estimated[0], c='r', marker="o", s=10, label="Estymowane b0 od Lambdy")  
-    ax_est.plot(t, B_true[:,0], c='black') 
+    ax_est.plot(t, [b_true[0] for _ in range(len(t))], c='black') 
     ax_est.scatter(t, b_estimated[1], c='b', marker="o", s=10, label="Estymowane b1 od Lambdy")
-    ax_est.plot(t, B_true[:,1], c='black') 
+    ax_est.plot(t, B_changed, c='black') 
     ax_est.scatter(t, b_estimated[2], c='g', marker="o", s=10, label="Estymowane b2 od Lambdy") 
-    ax_est.plot(t, B_true[:,2], c='black') 
+    ax_est.plot(t, [b_true[2] for _ in range(len(t))], c='black') 
     ax_est.set_title(f'Estymator b^  przy zmiennym w czasie b* dla Lambdy = {Lambda}')
     # ax_est.legend(loc='')
     ax_est.set_xlabel('Czas (s)')
@@ -235,20 +237,26 @@ def image(t, b_true, Lambda):
 
 
 def main():
-    duration = 500.0  # Duration of the u_k(seconds)
-    N = 250 # Amount of samples
+    # triangular function parameters 
+    frequency = 0.01  # Frequency of the signal (cycles per second)
+    period = 1 / frequency
+    amplitude = 1.0  # Amplitude of the signal
+
+    duration = 600.0  # Duration of the u_k(seconds)
+    N = 200 # Amount of samples
     t = np.linspace(0, duration, N, endpoint=False) # <class 'numpy.ndarray'>
     # u_k = np.random.uniform(0, 3,  N)
-
     Lambda = [1, 0.99, 0.95, 0.9, 0.8]
     b_true = [5, 3, 1]
     
+    B_true = ((4 * amplitude) / period) * np.abs(((t - (period * 0.25))%period) - (period * 0.5)) - amplitude + b_true[1]
+
     # Part I
     image(t, b_true, Lambda[0])
 
     # Part II
     for i in range(len(Lambda)):
-        image2(t, b_true, Lambda[i])
+        image2(t, b_true, B_true, Lambda[i])
     
 if __name__ == "__main__":
     main()
